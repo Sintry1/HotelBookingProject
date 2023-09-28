3fe219fae76ef7c702bb8aacf537ac123ed25296
ziakhan82 <ziaullahhassan8@gmail.com>
2023-09-28 19:08:17 +0200

    Added comments to the rest of the unit test code

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -66,7 +66,7 @@ namespace HotelBooking.UnitTests
             Assert.Empty(bookingForReturnedRoomId);
         }
 
-        // #1 This unit test is checking the behavior of the FindAvailableRoom method
+        // #1: This unit test is checking the behavior of the FindAvailableRoom method
         // when the start date is in the past and the end date is also in the past.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_StartAndEndInPast()
@@ -84,94 +84,132 @@ namespace HotelBooking.UnitTests
             Assert.Throws<ArgumentException>(act);
         }
 
-        // #2
+        // #2: This unit test is checking the behavior of the FindAvailableRoom method
+        // when the start date is in the past, but the end date is in the future.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_StartInPast()
         {
             // Arrange
-            DateTime startDate = DateTime.Today.AddDays(-1);
-            DateTime endDate = DateTime.Today.AddDays(5);
-            // Act
+            DateTime startDate = DateTime.Today.AddDays(-1); // Set a start date 1 day in the past.
+            DateTime endDate = DateTime.Today.AddDays(5);    // Set an end date 5 days in the future.
+
+            // Act: Call the FindAvailableRoom method with a start date in the past and an end date in the future,
+            // and capture any exception that might be thrown during this action.
             Action act = () => bookingManager.FindAvailableRoom(startDate, endDate);
+
             // Assert
+            // We expect that calling FindAvailableRoom with an invalid start date will throw an ArgumentException.
             Assert.Throws<ArgumentException>(act);
         }
 
-        // #3
+        // #3: This unit test is checking the behavior of the FindAvailableRoom method
+        // when there is a room available that is not fully booked within the specified date range.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_NotFullyBooked()
         {
             // Arrange
-            DateTime startDate = DateTime.Today.AddDays(1);
-            DateTime endDate = DateTime.Today.AddDays(5);
-            // Act
+            DateTime startDate = DateTime.Today.AddDays(1);  // Set a start date 1 day in the future.
+            DateTime endDate = DateTime.Today.AddDays(5);   // Set an end date 5 days in the future.
+
+            // Act: Call the FindAvailableRoom method with a start date in the future and an end date in the future,
+            // and retrieve the room ID assigned to the available room (if any).
             int roomId = bookingManager.FindAvailableRoom(startDate, endDate);
+
             // Assert
+            // We expect that a room ID other than -1 will be returned,
+            // indicating that there is an available room for the specified date range
             Assert.NotEqual(-1, roomId);
         }
 
-        // #4
+        // #4: This unit test is checking the behavior of the FindAvailableRoom method
+        // when the start date is before the end date, but all rooms are fully booked within the specified date range.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_StartBeforeEndAfterFullyBooked()
         {
             // Arrange
-            DateTime startDate = DateTime.Today.AddDays(8);
-            DateTime endDate = DateTime.Today.AddDays(22);
-            // Act
+            DateTime startDate = DateTime.Today.AddDays(8);  // Set a start date 8 days in the future.
+            DateTime endDate = DateTime.Today.AddDays(22);  // Set an end date 22 days in the future.
+
+            // Act: Call the FindAvailableRoom method with a start date in the future and an end date further in the future,
+            // and retrieve the room ID assigned to the available room (if any).
             int roomId = bookingManager.FindAvailableRoom(startDate, endDate);
+
             // Assert
+           // We expect that -1 will be returned, indicating that no available rooms exist within the specified date range.
             Assert.Equal(-1, roomId);
         }
 
-        // #5
+        // #5: This unit test is checking the behavior of the FindAvailableRoom method
+        // when both the start date and end date are after the specified date range is fully booked.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_StartAndEndAfterFullyBooked()
         {
             // Arrange
-            DateTime startDate = DateTime.Today.AddDays(22);
-            DateTime endDate = DateTime.Today.AddDays(26);
-            // Act
+            DateTime startDate = DateTime.Today.AddDays(22);  // Set a start date 22 days in the future.
+            DateTime endDate = DateTime.Today.AddDays(26);   // Set an end date 26 days in the future.
+
+            // Act: Call the FindAvailableRoom method with both a start date and an end date in the future,
+            // and retrieve the room ID assigned to the available room (if any).
             int roomId = bookingManager.FindAvailableRoom(startDate, endDate);
+
             // Assert
+            // We expect that a room ID other than -1 will be returned,
+            // indicating that there is an available room for the specified date range.
             Assert.NotEqual(-1, roomId);
         }
-        //testing
-        // #6
+
+        // #6: This unit test is checking the behavior of the FindAvailableRoom method
+        // when the start date is before the end date, but all rooms are fully booked during the specified date range.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_StartBeforeEndDuringFullyBooked()
         {
             // Arrange
-            DateTime startDate = DateTime.Today.AddDays(8);
-            DateTime endDate = DateTime.Today.AddDays(15);
-            // Act
+            DateTime startDate = DateTime.Today.AddDays(8); // Set a start date 8 days in the future.
+            DateTime endDate = DateTime.Today.AddDays(15);  // Set an end date 15 days in the future.
+
+            // Act: Call the FindAvailableRoom method with a start date in the future and an end date within the future,
+            // and retrieve the room ID assigned to the available room (if any).
             int roomId = bookingManager.FindAvailableRoom(startDate, endDate);
+
             // Assert
+            // We expect that -1 will be returned, indicating that no available rooms exist within the specified date range.
             Assert.Equal(-1, roomId);
         }
 
-        // #7
+        // #7: This unit test is checking the behavior of the FindAvailableRoom method
+        // when both the start date and end date fall within a specified date range that is fully booked.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_StartAndEndDuringFullyBooked()
         {
             // Arrange
-            DateTime startDate = DateTime.Today.AddDays(11);
-            DateTime endDate = DateTime.Today.AddDays(15);
-            // Act
+            DateTime startDate = DateTime.Today.AddDays(11);  // Set a start date 11 days in the future.
+            DateTime endDate = DateTime.Today.AddDays(15);   // Set an end date 15 days in the future.
+
+            // Act: Call the FindAvailableRoom method with both a start date and an end date in the future,
+            // and retrieve the room ID assigned to the available room (if any).
             int roomId = bookingManager.FindAvailableRoom(startDate, endDate);
+
             // Assert
+            // We expect that -1 will be returned, indicating that no available rooms exist within the specified date range.
             Assert.Equal(-1, roomId);
         }
 
-        // #8
+        // #8: This unit test is checking the behavior of the FindAvailableRoom method
+        // when the start date falls within a specified date range that is fully booked,
+        // but the end date is after the specified date range.
         [Fact]
         public void FindAvailableRoom_RoomAvailable_StartDuringEndAfterFullyBooked()
         {
             // Arrange
-            DateTime startDate = DateTime.Today.AddDays(15);
-            DateTime endDate = DateTime.Today.AddDays(22);
-            // Act
+            DateTime startDate = DateTime.Today.AddDays(15);  // Set a start date 15 days in the future.
+            DateTime endDate = DateTime.Today.AddDays(22);   // Set an end date 22 days in the future.
+
+            // Act: Call the FindAvailableRoom method with a start date within a fully booked date range
+            // and an end date in the future, and retrieve the room ID assigned to the available room (if any).
             int roomId = bookingManager.FindAvailableRoom(startDate, endDate);
+
             // Assert
+            // We expect that -1 will be returned, indicating that no available rooms exist within the specified date range.
             Assert.Equal(-1, roomId);
         }
 
