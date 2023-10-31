be989aa2c0048d55729695804d13a8642fa40597
Jesper Henning Lund <jesperlund9@hotmail.com>
2023-10-31 11:21:15 +0100

    Fuck specflow

--- SpecFlow.HotelBooking/Features/SpecFlowBookingManager.feature
+++ SpecFlow.HotelBooking/Features/SpecFlowBookingManager.feature
@@ -16,25 +16,29 @@ Example:
 
 @mytag
 Scenario: Booking before occupied range
-	Given I have input startDate 2023-11-10
-	And I have input endDate 2023-11-12
+	Given I have a booking request with the following details
+            | Start Date  | End Date    |
+            | 2023-11-01  | 2023-11-05  |
 	When I attempt booking before occupied range
 	Then Booking True
 
 Scenario: Booking After Occupied Range
-	Given I have input startDate 2023-12-01
-	And I have input endDate 2023-12-03
+Given I have a booking request with the following details
+            | Start Date  | End Date    |
+            | 2023-12-01  | 2023-12-03  |
 	When I attempt booking after occupied range
 	Then Booking True
 
 Scenario: No Booking During Occupied Range
-	Given I have input a startDate during Occupied Range
-	And I have input a endDate during Occupied Range
+	Given I have a booking request with the following details
+            | Start Date  | End Date    |
+            | 2023-11-26 | 2023-11-27  |
 	When I attempt booking during occupied range
 	Then Booking False
 
 Scenario: EndDate overlaps occupied range
-	Given I have input a startDate before Occupied Range
-	And I have input a endDate during Occupied Range
+	Given I have a booking request with the following details
+            | Start Date  | End Date    |
+            | 2023-11-01  | 2023-11-11  |
 	When I attempt booking during occupied range
 	Then Booking False
--- SpecFlow.HotelBooking/StepDefinitions/BookingStepDefinitions.cs
+++ SpecFlow.HotelBooking/StepDefinitions/BookingStepDefinitions.cs
@@ -3,63 +3,32 @@ using Moq;
 using System;
 using TechTalk.SpecFlow;
 
-namespace HotelBooking.Specflow.Booking.StepDefinitions
+namespace SpecFlow.HotelBooking.StepDefinitions
 {
     [Binding]
     public class BookingStepDefinitions
     {
-        DateTime startDate, endDate;
-        DateTime[] occupiedDates = new DateTime[]
-        {
-            new DateTime(2023,11,25),
-            new DateTime(2023,11,30),
-        };
-
-        IRepository<Booking> bookingRepository;
-        private Mock<IRepository<Booking>> bookingRepositoryMock;
-        private Mock<IRepository<Room>> roomRepositoryMock;
-        private IBookingManager bookingmanager = new BookingManager();
-
-        private ScenarioContext scenarioContext;
 
         [Given(@"occupiedRange")]
-        public void GivenOccupiedRange(Table table)
+        public void GivenOccupiedRange()
         {
             throw new PendingStepException();
         }
 
-        [Given(@"I have input a startDate before Occupied Range")]
-        public void GivenIHaveInputAStartDateBeforeOccupiedRange(DateTime bookingStartDate)
+        [Given(@"I have a booking request with the following details")]
+        public void GivenIHaveABookingRequestWithTheFollowingDetails(Table table)
         {
-            startDate = bookingStartDate;
-        }
-
-        [Given(@"I have input a endDate before Occupied Range")]
-        public void GivenIHaveInputAEndDateBeforeOccupiedRange(DateTime bookingEndDate)
-        {
-            endDate = bookingEndDate;
+            throw new PendingStepException();
         }
 
         [When(@"I attempt booking before occupied range")]
         public void WhenIAttemptBookingBeforeOccupiedRange()
         {
-            
-        }
-
-        [Then(@"A booking is made before occupied range")]
-        public void ThenABookingIsMadeBeforeOccupiedRange()
-        {
             throw new PendingStepException();
         }
 
-        [Given(@"I have input a startDate after Occupied Range")]
-        public void GivenIHaveInputAStartDateAfterOccupiedRange(DateTime startDate)
-        {
-            throw new PendingStepException();
-        }
-
-        [Given(@"I have input a endDate after Occupied Range")]
-        public void GivenIHaveInputAEndDateAfterOccupiedRange(DateTime endDate)
+        [Then(@"Booking True")]
+        public void ThenBookingTrue()
         {
             throw new PendingStepException();
         }
@@ -70,32 +39,14 @@ namespace HotelBooking.Specflow.Booking.StepDefinitions
             throw new PendingStepException();
         }
 
-        [Then(@"A booking is made after occupied range")]
-        public void ThenABookingIsMadeAfterOccupiedRange()
-        {
-            throw new PendingStepException();
-        }
-
-        [Given(@"I have input a startDate during Occupied Range")]
-        public void GivenIHaveInputAStartDateDuringOccupiedRange(DateTime startDate)
-        {
-            throw new PendingStepException();
-        }
-
-        [Given(@"I have input a endDate during Occupied Range")]
-        public void GivenIHaveInputAEndDateDuringOccupiedRange(DateTime endDate)
-        {
-            throw new PendingStepException();
-        }
-
         [When(@"I attempt booking during occupied range")]
         public void WhenIAttemptBookingDuringOccupiedRange()
         {
             throw new PendingStepException();
         }
 
-        [Then(@"no booking is made")]
-        public void ThenNoBookingIsMade()
+        [Then(@"Booking False")]
+        public void ThenBookingFalse()
         {
             throw new PendingStepException();
         }
